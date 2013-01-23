b045a2398f669b195d76180944c8d0e561bf3d82
Lance Speelmon <lance@speelmon.com>
2013-01-22 21:55:55 -0700

    Started migration of custom git prompt to a zsh theme

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -3,7 +3,6 @@ alias hibernateoff="sudo pmset -a hibernatemode 0"
 alias ll='ls -lGh'
 alias l='ls -laGh'
 alias lt='ls -ltGh'
-alias md='mkdir'
 alias deploy-mvn-artifacts='mvn clean install javadoc:jar source:jar deploy'
 alias tomcat_start='./bin/startup.sh;tail -F logs/catalina.out'
 alias jpda_start='export JPDA_ADDRESS=8000;export JPDA_TRANSPORT=dt_socket;./bin/catalina.sh jpda start;tail -F logs/catalina.out'
